5445bf9fd04126ddb62764241b67eec15bb2e339
urrik98 <adamrenschen@gmail.com>
2018-07-19 11:27:26 -0700

    Add navbar, early routing to home and biorepository. Add bootstrap gem and configure application.scss and .js

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+  def index
+
+  end
+
+  def biorepository
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  get '/biorepository', to: 'home#biorepository', as: 'biorepository' #this formats the request. get "URL in address bar", to "controller/action", as "rails path name"
 end
