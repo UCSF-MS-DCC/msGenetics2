e16b2c93e65a054a1e72e9e28c0ec08faa0a8b07
urrik98 <adamrenschen@gmail.com>
2018-10-19 12:42:19 -0700

    ...

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,6 +32,7 @@ set :console_user, :deployment
 # set :format,        :pretty
 # set :log_level,     :debug
 set :keep_releases, 5
+set :linked_filers, %w(config/master.key)
 
 ## Linked Files & Directories (Default None):
 # set :linked_files, %w{config/database.yml}
