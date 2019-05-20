85eb778af41eae8138cba8249144456fd3f162c5
urrik98 <adamrenschen@gmail.com>
2019-05-20 09:31:15 -0700

    Update Gemfile to ruby 2.5.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,10 +33,9 @@ set :console_user, :deployment
 # set :log_level,     :debug
 set :keep_releases, 5
 set :linked_filers, %w(config/master.key)
-
 ## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+ #set :linked_files, %w{config/database.yml}
+ #set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
