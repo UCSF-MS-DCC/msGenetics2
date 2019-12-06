8cc57862ed6bf4ac373eaafe698dc5fa1ba0719c
urrik98 <adamrenschen@gmail.com>
2019-12-06 12:04:11 -0800

    Add Jorge email to recipients of sample request notifications

--- app/mailers/biorepository_notification_mailer.rb
+++ app/mailers/biorepository_notification_mailer.rb
@@ -1,10 +1,11 @@
 class BiorepositoryNotificationMailer < ApplicationMailer
   def samples_request_notification
     @customer = params[:customer]
-    if Rails.env == 'development'
-      mail(to: 'adam.renschen@ucsf.edu', subject:"Request for samples information from: #{@customer.email}")
-    else
-      mail(to: 'biorepository@ucsf.edu, adam.renschen@ucsf.edu', subject:"Request for samples information from: #{@customer.email}")
-    end
+    # if Rails.env == 'development'
+    #   mail(to: 'adam.renschen@ucsf.edu', subject:"Request for samples information from: #{@customer.email}")
+    # else
+    #   mail(to: 'biorepository@ucsf.edu, adam.renschen@ucsf.edu, jorge.oksenberg@ucsf.edu', subject:"Request for samples information from: #{@customer.email}")
+    # end
+    mail(to: 'biorepository@ucsf.edu, adam.renschen@ucsf.edu, jorge.oksenberg@ucsf.edu', subject:"Request for samples information from: #{@customer.email}")
   end
 end
