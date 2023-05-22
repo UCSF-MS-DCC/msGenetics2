28fc7b077e3d323572d3fa1a0151c83dce87c6fd
Adam Renschen <adamrenschen@gmail.com>
2023-05-22 15:25:43 -0700

    Upgrade application to Rails 6.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
   get '/biorepository', to: 'home#biorepository', as: 'biorepository' #this formats the request. get "URL in address bar", to "controller/action", as "rails path name"
   get '/dashboard', to: 'home#dashboard', as: 'dashboard'
